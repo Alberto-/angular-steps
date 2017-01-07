d5e4047ddf4db6c9daee19be451ebae81ee9b54e
Alberto Piras <a.piras.ict@gmail.com>
2017-01-07 11:36:34 +0100

    updating status 1

---
+++ app/controllers/step1Controller.js
@@ -0,0 +1,26 @@
+registrationApp.controller('step1Controller',['$scope','$http','sharedService', 'staticPropertiesService', '$location', function ($scope, $http, sharedService, staticPropertiesService, $location) {
+
+        $scope.reg = sharedService;
+        $scope.utils = staticPropertiesService;
+        $scope.isCompany = false;
+
+
+        $scope.societaPattern = $scope.utils.societaCustom;
+        $scope.namePattern = $scope.utils.namePattern;
+        $scope.emailPattern = $scope.utils.emailPattern;
+        $scope.datePattern = $scope.utils.datePattern;
+    
+        
+        $scope.checkStep1 = function () {
+            console.info("Check to move step2");
+            if ($scope.formStep1.$valid) {
+                console.log("Step2 unlocked");
+                $scope.reg.enableStep(2);
+                $scope.moveNextStep(2);
+            }else{
+                console.error("Form Step1 not valid");
+            }
+        };
+
+    }]);
+
